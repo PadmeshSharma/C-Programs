dab82462c7ff3b5bb24c9877bbe5a8ff4040d2bf
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2022-04-05 20:51:36 +0530

    Create sum of no.s

---
+++ sum of no.s
@@ -0,0 +1,23 @@
+// FOR SUM OF INPUT NO.S
+
+#include<stdio.h>
+int main()
+{
+	int a, b, sum;
+	printf("Enter two no.s");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("Sum of these two no.s is %d",sum);
+	return 0;
+}
+
+\\FOR SUM OF PARTICULAR NO.S
+#include<stdio.h>
+int main()
+{
+	int a=2;
+  int b= 3;
+  sum=a+b;
+	printf("Sum of these two no.s is %d",sum);
+	return 0;
+}
