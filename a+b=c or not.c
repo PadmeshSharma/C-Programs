4c3725eb339191d55b1389b24c5d736aca9a9374
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2022-08-11 07:42:27 +0530

    Add files via upload

---
+++ a+b=c or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter three no.s");
+	scanf("%d%d%d",&a,&b,&c);
+	if (c==a+b)
+		{
+		printf("True");
+		}
+	else
+		{
+		printf("False");
+	    }
+	return 0;
+}
